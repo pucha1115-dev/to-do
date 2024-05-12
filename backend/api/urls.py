5b4ba43a82e4cd7cc47ecb7e9b57df18f2c5a7f1
jp-bns2020 <jp.bns2020@gmail.com>
2024-05-12 19:18:51 +0800

    disregard generics

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TodoListCreateView, TodoDeleteView
+from .views import TodoListView, TodoDeleteView, TodoUpdateView
 
 urlpatterns = [
-    path('todos/', TodoListCreateView.as_view(), name="todo_list"),
-    path('todos/delete/<int:pk>', TodoDeleteView.as_view(), name="todo_delete")
+    path('todos/', TodoListView.as_view(), name="todo_list"),
+    path('todos/update/<int:pk>/', TodoUpdateView.as_view(), name="todo_delete"),
+    path('todos/delete/<int:pk>/', TodoDeleteView.as_view(), name="todo_delete"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -4,31 +4,60 @@ from django.contrib.auth.models import User
 from .models import Todo
 from .serializer import UserSerializer, TodoSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
-class TodoListCreateView(generics.ListCreateAPIView):
-    serializer_class = TodoSerializer
+class TodoListView(APIView):
     permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user # get the user that is authenticated
-        todos = Todo.objects.filter(author=user) # get all note from the authenticated user and return it
-        return todos
     
-    def perform_create(self, serializer): # access the serializer_class which is NoteSerializer
-        if serializer.is_valid():  # check if serializztion is valid
-            serializer.save(author=self.request.user) # add the author to the note
-
-class TodoDeleteView(generics.DestroyAPIView):
-    serializer_class = TodoSerializer
+    def get(self, request): # get the logged in user
+        user = self.request.user
+        todos = Todo.objects.filter(author=user) # get all todos that belongs to the logged in user
+        serializer = TodoSerializer(todos, many=True)    
+        return Response(serializer.data)    
+    
+    def post(self, request):
+        user = self.request.user # get the logged in user
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid() :
+            serializer.save(author=user); # add the logged in user as author
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+   
+    
+class TodoUpdateView(APIView):
+     permission_classes = [IsAuthenticated]
+     
+     def put(self, request, pk):
+        user =self.request.user # get the logged in user
+        todo = Todo.objects.get(author=user, pk=pk) # get the specific todo
+        serilaizer = TodoSerializer(todo, data=request.data) # update the todo
+        if serilaizer.is_valid():
+            serilaizer.save()
+            return Response(serilaizer.data)
+        return Response(serilaizer.errors)
+        
+    
+class TodoDeleteView(APIView):
     permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-      user = self.request.user
-      return Todo.objects.filter(author=user)
-
-class CreateUserView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    
+    def delete(self, request, pk): # get the logged in user
+        user = self.request.user
+        todo = Todo.objects.filter(author=user, pk=pk)
+        todo.delete()
+        return Response({"status": "deleted"})
+    
+    
+class CreateUserVIew(APIView):
     permission_classes = [AllowAny]
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+        
