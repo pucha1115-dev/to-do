3347f8f9224ec6acbb2361ad53dd85a68245f607
pucha1115-dev <jp.bns2020@gmail.com>
2024-05-17 14:21:27 +0800

    test commit

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -5,9 +5,6 @@ from django.core.mail import send_mail
 from django.conf import settings
 import uuid
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from django.core.mail import send_mail
-from django.conf import settings
 from django.utils.crypto import get_random_string
 
 
