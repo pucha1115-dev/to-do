6351b05afc2cc6b89e3afb0b73261a5329e9d4cf
jp-bns2020 <jp.bns2020@gmail.com>
2024-05-21 00:35:52 +0800

    delete other files

--- frontend/bkup/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-    return (
-        <div><h1 className='text-center mb-5'>Welcome to TodoTrek</h1></div>
-    )
-}
-
-export default Header;
--- frontend/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-    return (
-        <div><h1 className='header text-center mb-5 mt-5'>TodoTrek</h1></div>
-    )
-}
-
-export default Header;
